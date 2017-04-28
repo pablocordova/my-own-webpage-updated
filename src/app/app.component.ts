e76365e79c3d03bc07c1de936dca86395fdec6c8
Pablo Cordova Morales <pccm_77@hotmail.com>
2017-04-28 00:02:30 -0500

    Response form showed to user and rustic validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,25 +23,54 @@ export class AppComponent {
 
     data: string;
 
-    myContact = new Contact('fname','sname','email','message');
+    myContact = new Contact('','','','');
+
+    // boolean message to check if the data is saved correctly
+
+    successMessage = false;
+
+    // Error message case not fill out the fields.
+    errorMessage = false;
 
     constructor(private http: Http) { }
 
     ngOnInit() {
+        // Make the video with infinite loop
         this.video = document.getElementById('video-welcome');
         this.video.loop = true;
         this.video.load();
     }
 
     saveForm() {
+
+        // Validate form, I am not using a directive of angular yet.
+        // TODO: use directive of angular to validate correctly.
+
+        if(this.myContact['fname'] == '' || 
+            this.myContact['sname'] == '' ||
+            this.myContact['email'] == '' || 
+            this.myContact['message'] == '') {
+                this.errorMessage = true;
+                return;
+        }
+        else {
+            this.errorMessage = false;
+        }
+
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
         let requestOptions = new RequestOptions({ headers: headers });
         
+        // Encode JSON as string to exchange data to web server.
         this.data = JSON.stringify(this.myContact);
-        console.log(this.data);
 
-        this.http.post('http://localhost:3000/api', this.data, requestOptions).subscribe(res => console.log(res.json()));
+        this.http.post('http://localhost:3000/api', this.data, requestOptions).subscribe(res => {
+                let result = res.json();
+                if(result['result'] == 'success') {
+                    this.successMessage = true;
+                }
+            }
+        );
 
     }
 }
